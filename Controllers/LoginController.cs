8412130233d477eb1c5746325bdaef65ad42e426
“Your <pes.andrea@outlook.it>
2024-03-04 16:13:23 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using AgenziaCheSpedisce.Models;
+
+namespace AgenziaCheSpedisce.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User User)
+        {
+            var hasValidCredentials = false;
+
+            hasValidCredentials = true;
+
+            if (hasValidCredentials)
+            {
+                FormsAuthentication.SetAuthCookie(User.UserId.ToString(), true);
+                return RedirectToAction("", "");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
